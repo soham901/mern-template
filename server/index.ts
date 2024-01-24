5a4531d81cbeba73845ef2111536fd81b3146252
soham <sagathiyasoham12345@gmail.com>
2024-01-24 09:34:46 +0530

    Integrated MongoDB with mongoose

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import mongoose from "mongoose";
 
+// Load environment variables
 const PORT = process.env.PORT || 8000;
+const DB_URL = process.env.DB_URL || "mongodb://127.0.0.1:27017/express-ts";
 
 const app = express();
 
@@ -8,6 +11,17 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+// Connect to DB and start server
+mongoose
+  .connect(DB_URL)
+  .then(() => {
+    console.log("Connected to DB");
+
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error connecting to DB");
+    console.log("err: ", err);
+  });
